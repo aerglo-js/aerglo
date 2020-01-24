043438103cbe10eedd60dd96d9461f36bd3a415e
Zane Milakovic <zane.milakovic@icloud.com>
2020-01-24 10:04:55 -0600

    breaked down my thoughts on the general flow

--- src/index.ts
+++ src/index.ts
@@ -48,14 +48,40 @@ const test = async () => {
     });
 
 
-    // Need to change from Instance and Module, to Client and Build?
-    // Marks a change from Svelte syntax, but they do different things
-    // Client would be exposed on the client itself
-    // It could support <script lang="typescript"></script>
-    // Which would allow it to be preprocessed
-    // And the output would then be bundled somehow on the client
-    // Where <script context="build"></script> would signify that
-    // it only ran on the lambda, or the CI environment
+    // Thoughts -
+
+    // 1. Parser should return Acron parse of <script context="build"></script>, if presnt
+    // 2. Find AST parser for Styles, should be the same as Svelte if possible
+    // 3. Parser should return "???" parse of <style></style>, if present
+    // 4. Record lang or language attribute for <script context="client"></script>, if present
+    //    a. typescript
+    //    b. babel
+    // 5. Loop through Parse5 output and identify conditionals, and other Svelte defines
+    //    a. Should replace '#text' nodes that are conditionals to new types
+    //    b. Identify other '#text' nodes that Svelte defines that we need as well
+    // 6. Record lang or language attribute for <style></style>, if presnt
+    //    a. saas
+    //    b. less
+    //    c. stylus
+    // 7. Change "module" and "instance" to "client" and "build" scripts
+    //    NOTE - Need to change from Instance and Module, to Client and Build?
+    //    Marks a change from Svelte syntax, but they do different things
+    //    Client would be exposed on the client itself
+    //    It could support <script lang="typescript"></script>
+    //    Which would allow it to be preprocessed
+    //    And the output would then be bundled somehow on the client
+    //    Where <script context="build"></script> would signify that
+    //    it only ran on the lambda, or the CI environment
+    // 8. Split out CLI commands better
+    // 9. Make parser about processing a single file, compiler would handle the actual includes, props, etc
+    // 10. Need to break out mechanism to loop over set of files
+    // 11. Need to identify "custom components"
+    // 12. Need to replace Parse5 node of custom components, with new node
+    // 13. Custom components need to identify props and any special instructions for compiler
+    //     a. Variable replacement "{variable}"
+    //     b. JS Expression "{variable * 2}"
+    //     c. Are there any props outside of passed in ones that are valid?
+
     console.log('HTML (ast) - ', html);
     console.log('CSS (ast) - ', css);
     console.log('INSTANCE (ast) - ', instance);
