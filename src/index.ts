5668cb65e32bd1a14188d7e3ee8946069805e591
Zane@insideRX.com <zaneinsiderx.com@IRXWM99GTFM.local>
2020-01-30 11:23:11 -0600

    fixes files being processed even though params were incorrect

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,38 @@ import parser from './parser';
 import { setLoggingLevel, log, file } from './utils';
 
 program
-  .version('0.0.1')
-  .option('-f, --file <file>', 'Process an individual page file')
-  .option('-l, --log <log>', 'Logging level [1 (less) - 3 (more)] verbose, defaults to 1', '1')
+	.version('0.0.1')
+	.option('-f, --file <file>', 'Process an individual page file')
+	.option(
+		'-l, --log <log>',
+		'Logging level [1 (less) - 3 (more)] verbose, defaults to 1',
+		'1'
+	);
 
 program.parse(process.argv);
 
-// Sets logging level
-const level = parseInt(program.log, 10);
-if (!program.log || level < 1 || level > 3) {
-  log.error('Logging level must be 1, 2, or 3');
-} else {
-  setLoggingLevel(level);
-}
+// Parse defaults
+const level = parseInt(program.log, 10); // Logging Level
 
-// Process a single file
-if (program.file) {
-  const document = file.open(program.file);
-  if (typeof document === 'string') parser(document);
-}
+const invalidParams = (): number => {
+	let errors = 0;
 
+	// Validates program.log (1-3)
+	if (!program.log || level < 1 || level > 3) {
+		log.error('Logging level must be 1, 2, or 3');
+		errors++;
+	}
+
+	return errors;
+};
+
+if (!invalidParams()) {
+	// Sets logging level
+	if (program.log) setLoggingLevel(level);
+
+	// Process a single file
+	if (program.file) {
+		const document = file.open(program.file);
+		if (typeof document === 'string') parser(document);
+	}
+}
